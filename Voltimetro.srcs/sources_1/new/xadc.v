4463928cebfae4bd56e7dd2345212a322b5d17bb
aalexff <alexfigueroahrdz@gmail.com>
2022-09-24 17:11:17 -0600

    xadc v0.2.1
    Se agrega el template con los puertos a utilizar.

--- Voltimetro.srcs/sources_1/new/xadc.v
+++ Voltimetro.srcs/sources_1/new/xadc.v
@@ -41,30 +41,30 @@ module xadc(
           )
 
     XADC_inst (	// ALARMS: 8-bit (each) output: ALM, OT
-                    .ALM(), // 8-bit output: Output alarm for temp, Vccint, Vccaux and Vccbram
-                    .OT(), // 1-bit output: Over-Temperature alarm
+                //    .ALM(), // 8-bit output: Output alarm for temp, Vccint, Vccaux and Vccbram
+                //    .OT(), // 1-bit output: Over-Temperature alarm
                 // Dynamic Reconfiguration Port (DRP): 16-bit (each) output: Dynamic Reconfiguration Ports
-                    .DO(reading), // 16-bit output: DRP output data bus
-                    .DRDY(), // 1-bit output: DRP data ready
+                //    .DO(reading), // 16-bit output: DRP output data bus
+                //    .DRDY(), // 1-bit output: DRP data ready
                 // STATUS: 1-bit (each) output: XADC status ports
-                    .BUSY(), // 1-bit output: ADC busy output
-                    .CHANNEL(), // 5-bit output: Channel selection outputs
-                    .EOC(), // 1-bit output: End of Conversion
-                    .EOS(), // 1-bit output: End of Sequence
-                    .JTAGBUSY(), // 1-bit output: JTAG DRP transaction in progress output
-                    .JTAGLOCKED(), // 1-bit output: JTAG requested DRP port lock
-                    .JTAGMODIFIED(), // 1-bit output: JTAG Write to the DRP has occurred
-                    .MUXADDR(), // 5-bit output: External MUX channel decode
+                //    .BUSY(), // 1-bit output: ADC busy output
+                //    .CHANNEL(), // 5-bit output: Channel selection outputs
+                //    .EOC(), // 1-bit output: End of Conversion
+                //    .EOS(), // 1-bit output: End of Sequence
+               //     .JTAGBUSY(), // 1-bit output: JTAG DRP transaction in progress output
+                //    .JTAGLOCKED(), // 1-bit output: JTAG requested DRP port lock
+                //    .JTAGMODIFIED(), // 1-bit output: JTAG Write to the DRP has occurred
+                //    .MUXADDR(), // 5-bit output: External MUX channel decode
                 // Auxiliary Analog-Input Pairs: 16-bit (each) input: VAUXP[15:0], VAUXN[15:0]
-                    .VAUXN(vauxn), // 16-bit input: N-side auxiliary analog input
-                    .VAUXP(vauxp), // 16-bit input: P-side auxiliary analog input
+                //    .VAUXN(vauxn), // 16-bit input: N-side auxiliary analog input
+                //    .VAUXP(vauxp), // 16-bit input: P-side auxiliary analog input
                 // CONTROL and CLOCK: 1-bit (each) input: Reset, conversion start and clock inputs
-                    .CONVST(), // 1-bit input: Convert start input
-                    .CONVSTCLK(), // 1-bit input: Convert start input
+                //    .CONVST(), // 1-bit input: Convert start input
+                //   .CONVSTCLK(), // 1-bit input: Convert start input
                     .RESET(RST), // 1-bit input: Active-high reset
                 // Dedicated Analog Input Pair: 1-bit (each) input: VP/VN
-                    .VN(), // 1-bit input: N-side analog input
-                    .VP(), // 1-bit input: P-side analog input
+               //     .VN(), // 1-bit input: N-side analog input
+               //     .VP(), // 1-bit input: P-side analog input
                 // Dynamic Reconfiguration Port (DRP): 7-bit (each) input: Dynamic Reconfiguration Ports
                     .DADDR(8'h16), // 7-bit input: DRP address bus
                     .DCLK(CLK_100), // 1-bit input: DRP clock
